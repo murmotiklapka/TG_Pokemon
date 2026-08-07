0deb81eb7947d915cc7c53116bb77e37691ae203
murmotiklapka <murmotiklapka@gmail.com>
2026-08-07 19:19:02 +0200

    Implement Pokémon healing feature and improve commands
    Added healing functionality for the Pokémon with a new /heal command. Enhanced the existing commands to provide additional messages and checks.

--- main.py
+++ main.py
@@ -15,11 +15,12 @@ def go(message):
         bot.send_photo(message.chat.id, pokemon.show_img())
         bot.send_message(message.chat.id, pokemon.show_nummber())
         bot.send_video(message.chat.id, pokemon.show_animation())
+        bot.send_message(message.chat.id, 'вы можете подлеить покемона командой /heal')
     else:
         bot.reply_to(message, "Ты уже создал себе покемона")
 
 @bot.message_handler(commands=['pikachu'])
-def go(message):
+def pikachu(message):
     global pikachu_nummer
     if pikachu_nummer == 0:
         pokemon = Pokemon(message.from_user.username, 25)
@@ -32,6 +33,23 @@ def go(message):
     else:
         bot.reply_to(message, "Ты уже создал себе пикачу")
 
-bot.infinity_polling(none_stop=True)
+@bot.message_handler(commands=['heal'])
+def heal(message):
+    global pokemon
+    pokemon_hp = pokemon.hp
+    if pokemon.hp < pokemon.maxhp:
+        if pokemon.bonus > 0:
+            heal_num = pokemon.maxhp - pokemon.hp
+            if pokemon.bonus >= heal_num:
+                pokemon.hp += pokemon.bonus
+                pokemon.bonus -= heal_num
+            else:
+                pokemon.hp += pokemon.bonus
+                pokemon.bonus -= pokemon.bonus
+            heel_last_num = pokemon.hp - pokemon_hp
+            bot.send_message(message.chat.id, f'Вы подлечили своего покемона на {heel_last_num} хп. Теперь у него {pokemon.hp} хп')
+        else:
+            bot.send_message(message.chat.id, f'У вашего покемона нет бонуса для лечения. У него {pokemon.hp} хп. Для получения бонуса выйграйте бой')
 
+bot.infinity_polling(none_stop=True)
 
